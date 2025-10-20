6384ba947174c9c4369961ee117599db2ccba1b8
HLJDanni <hljdanni2023@gmail.com>
2025-10-20 10:41:22 -0600

    Se reorganizó las carpetas de monitores

--- src/main/java/com/ControladoresHTML/Monitores/PowerBIControllerControl_Calidad.java
+++ src/main/java/com/ControladoresHTML/Monitores/PowerBIControllerControl_Calidad.java
@@ -1,4 +1,4 @@
-package com.coffee.coffee.Monitores;
+package com.ControladoresHTML.Monitores;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
