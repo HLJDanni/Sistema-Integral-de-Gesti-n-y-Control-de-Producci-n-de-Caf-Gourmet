dbe5708729f4733c60add429a7bee530a03fe674
bdiazr7-arch <bdiazr7@miumg.edu.gt>
2025-10-21 01:22:11 -0600

    Update App.jsx
    agregamos los paths del nuevo panel y actualizamos el path de login

--- Sistema-Integral-de-Gesti-n-y-Control-de-Producci-n-de-Caf-Gourmet-frontend/src/App.jsx
+++ Sistema-Integral-de-Gesti-n-y-Control-de-Producci-n-de-Caf-Gourmet-frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import Login from "./features/auth/Login/Login";
+import Login from "./features/auth/pages/Login";
+import DashboardLayout from "./features/Dashboard/pages/DashboardLayout";
 import Dashboard from "./features/Dashboard/pages/Dashboard";
+import Granos from "./features/Dashboard/Granos/Granos";
 
 
 
@@ -12,7 +14,11 @@ function App() {
       <Toaster position="top-right" />
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+       <Route path="/dashboard/*" element={<DashboardLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="Granos" element={<Granos />} />
+         
+        </Route>
       </Routes>
     </BrowserRouter>
   );
